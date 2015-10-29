cfd9d216af6b541f8cc5abdf1757bd8107392bb8
pepe-roni <ahde2k@gmail.com>
2015-10-29 10:24:36 -0700

    nbfvjh

--- Chick.java
+++ Chick.java
@@ -1,4 +1,27 @@
 class Chick implements Animal 
 {     
-     //your code here
+  private String myType;     
+  private String mySound;      
+  public Chick(String type, String sound, String sound2) 
+  {         
+    myType = type;         
+    if(Math.random()*1<0.5)
+    {
+      mySound=sound2;
+    }
+    else
+    mySound=sound;
+  }     
+  public Chick() 
+  {         
+    myType = "unknown";         
+    mySound = "unknown";
+  }      
+  public String getSound() {
+    return mySound;
+  }     
+  public String getType() {
+    return myType;
+  }
 }
+
--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,15 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+  private String pname;
+  public NamedCow(String type, String name, String sound)
+  {
+    pname=name;
+    myType = type;         
+    mySound = sound;   
+  }
+  public String getName()
+  {
+    return pname;
+  }
 }
+
